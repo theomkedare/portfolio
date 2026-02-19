d2b29593c58ad7137c1a589962881f8545523cce
Om Kedare <omkedare65@gmail.com>
2026-02-19 23:41:54 +0530

    minor mistake

--- portfolio2/src/app/sitemap.ts
+++ portfolio2/src/app/sitemap.ts
@@ -1,50 +1,11 @@
-// app/sitemap.ts
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
-      url: "https://omkedare.dev",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: "https://omkedare.dev/projects",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://omkedare.dev/certificates",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://omkedare.dev/contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://omkedare.dev/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://omkedare.dev/certificates",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://omkedare.dev/skills",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    // add any other pages like /projects, /about etc.
+    { url: "https://omkedare.dev", lastModified: new Date(), changeFrequency: "monthly", priority: 1 },
+    { url: "https://omkedare.dev/projects", lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
+    { url: "https://omkedare.dev/certificates", lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
+    { url: "https://omkedare.dev/skills", lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
+    { url: "https://omkedare.dev/contact", lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
   ];
 }
